de3f4aee967f8608c214e47db72fd0cbca4436d5
losevo <varenikda@gmail.com>
2021-11-18 14:07:12 +0300

    fourth step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 
-import welcome from "../src/cli.js";
+import welcome from '../src/cli.js';
 
 welcome();
